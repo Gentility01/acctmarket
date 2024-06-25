cd06918d7262cb863406f768c45db40488bb9471
Gentility01 <mastergentility5@gmail.com>
2024-06-25 16:24:37 +0100

    updates

--- render.sh
+++ render.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 # Your series of commands
-exec python manage.py migrate
+python manage.py collectstatic --noinput
+python manage.py compress
+python manage.py spectacular --color --file schema.yml
+python manage.py migrate
 # exec /usr/local/bin/gunicorn config.asgi --bind 0.0.0.0:5000 --chdir=/app -k uvicorn.workers.UvicornWorker
 exec python manage.py runserver 0.0.0.0:5000
